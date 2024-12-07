c2274df093347474cb8ab5c72f1cef6c7c15b621
Yogesh Singh <116149692+yogeshsingh-11@users.noreply.github.com>
2024-12-07 06:24:12 +0530

    Update option_pricing.py

--- Main/option_pricing.py
+++ Main/option_pricing.py
@@ -1,1 +1,12 @@
+import numpy as np
 
+def black_scholes_call(S0, K, T, r, sigma):
+    """
+    Black-Scholes formula for a European call option price.
+    return: Call option price
+    """
+    d1 = (np.log(S0 / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    
+    call_price = S0 * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+    return call_price
